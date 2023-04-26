69d08d3fd385c474b3cf2629494addd73e2f5761
dohh0117 <126572290+dohh0117@users.noreply.github.com>
2023-04-26 22:57:49 +0900

    Add files via upload

--- js/prot.js
+++ js/prot.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
                     var scrollTop = $(document).scrollTop();
                     if(scrollTop > 1800 && scene01Num == 1) {
                         scene01Num = 0;
-                        $('#skill .in_box01').stop().delay(0).css({top:'400px'}).animate({top:'150px'},1000);
-                        $('#skill .in_box02').stop().delay(500).css({top:'400px'}).animate({top:'190px'},1000);
-                        $('#skill .in_box03').stop().delay(1000).css({top:'400px'}).animate({top:'200px'},1000);
-                        $('#skill .in_box04').stop().delay(1500).css({top:'400px'}).animate({top:'230px'},1000);
+                        $('#skill .in_box01').stop().delay(0).css({left:'400px'}).animate({left:'90px'},1000);
+                        $('#skill .in_box02').stop().delay(500).css({left:'400px'}).animate({left:'220px'},1000);
+                        $('#skill .in_box03').stop().delay(1000).css({left:'400px'}).animate({left:'230px'},1000);
+                        $('#skill .in_box04').stop().delay(1500).css({left:'400px'}).animate({left:'290px'},1000);
                     }
                     else if(scrollTop < 1500 && scene01Num == 0){
                         scene01Num = 1;
-                        $('#skill .in_box01').stop().css({top:'150px'}).animate({top:'400px'},1000);
-                        $('#skill .in_box02').stop().css({top:'190px'}).animate({top:'400px'},1000); 
-                        $('#skill .in_box03').stop().css({top:'200px'}).animate({top:'400px'},1000);
-                        $('#skill .in_box04').stop().css({top:'230px'}).animate({top:'400px'},1000);
+                        $('#skill .in_box01').stop().css({left:'90px'}).animate({left:'400px'},1000);
+                        $('#skill .in_box02').stop().css({left:'220px'}).animate({left:'400px'},1000); 
+                        $('#skill .in_box03').stop().css({left:'230px'}).animate({left:'400px'},1000);
+                        $('#skill .in_box04').stop().css({left:'290px'}).animate({left:'400px'},1000);
 
                     }
                 });
