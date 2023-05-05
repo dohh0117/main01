882283c7bf10b3ad1bc6212e1005bdd23f4ab53e
dohh0117 <126572290+dohh0117@users.noreply.github.com>
2023-05-05 15:15:42 +0900

    Add files via upload

--- js/prot.js
+++ js/prot.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+   //----------------------------------------------------------------- 
+        var menu = $('.nav > li');
+        var contents = $('#wrap > div');  
+
+        $('.nav> li').click(function(event){
+            event.preventDefault();
+            
+            var tg = $(this);
+            var i = tg.index();
+            
+            var section = $('#wrap > div').eq(i);
+            var tt = section.offset().top;
+            
+            $('html, body').stop().animate({scrollTop:tt},1000);})
+       
+
+    //-------------------------------------------------------------------
     var scene01Num = 1;
 
                 $(window).scroll(function(){
